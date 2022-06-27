7c63df3f96aa0455f1ce381189379086297a9db8
최태호 <xogh136@naver.com>
2022-06-27 17:28:08 +0900

    Changes

--- src/main/java/com/example/notic/NoticApplication.java
+++ src/main/java/com/example/notic/NoticApplication.java
@@ -2,8 +2,9 @@ package com.example.notic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.notic")
 public class NoticApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/notic/controller/noticController.java
+++ src/main/java/com/example/notic/controller/noticController.java
@@ -2,6 +2,7 @@ package com.example.notic.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,4 +14,6 @@ public class noticController {
     public String notictest(){
         return "test";
     }
+
+
 }
