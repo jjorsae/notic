2f0e48f3cc24362f8ea21d6ea5118a52726e2ec5
최태호 <xogh136@naver.com>
2022-07-11 16:25:50 +0900

    Changes

--- src/main/java/com/example/notic/controller/noticController.java
+++ src/main/java/com/example/notic/controller/noticController.java
@@ -4,16 +4,39 @@ package com.example.notic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class noticController {
 
     @GetMapping("/test")
-    public String notictest(){
+    public String notictest(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
         return "test";
     }
 
+    @GetMapping("/test-api")
+    @ResponseBody//기본 json으로 변환해서 나타내준다 그냥 문자면 String converter가 동작 데이터 타입이면 jsonconverter
+    public Apiclass testapi(@RequestParam("name") String name, Model model){
+        Apiclass api = new Apiclass();
+        api.setName(name);
+        return api;
+    }
 
+
+
+
+
+    static class Apiclass{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
